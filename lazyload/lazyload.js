302c08ba7dc62dd8c009eb3f421db1c1962c872f
Pearyman <liyananhappy@sina.cn>
2015-07-08 11:23:33 +0800

    add a css3 swipe

---
+++ lazyload/lazyload.js
@@ -0,0 +1,53 @@
+/*
+ * used in the phone lazyload
+ * author:nan
+ * email:liyananhappy@sina.cn
+ * time:2015-5-1
+ */
+
+ define(function(){
+ 		var LL=function(){
+
+ 		}
+
+ 		LL.prototype={
+ 			init:function(){
+ 				var self=this;
+ 				options:{
+
+ 				}
+ 			}
+ 		}
+
+ })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ require-zepto-config.js
@@ -0,0 +1,15 @@
+//baseUrl 放在服务端写入页面
+require.config({
+	paths: {
+		// base: 'gallery/base/base',
+		// h5_base: 'gallery/h5_base/base',
+		// swipeslide : 'moudle/ui/h-swipeslide/swipeslide',
+		// lazyload: 'moudle/ui/lazyload/lazyload',
+        // waterfall: 'moudle/ui/waterfall/waterfall'
+        lazyload:'lazyload/lazyload'
+	},
+	shim : {
+
+	},
+	packages: []
+});
