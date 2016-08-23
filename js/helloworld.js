2de296adb097253e8f8a003a78978364fc07b5a8
Pearyman <liyananhappy@sina.cn>
2016-08-23 11:47:20 +0800

    react

---
+++ js/helloworld.js
@@ -0,0 +1,12 @@
+/**
+ * React commentBox test
+ */
+
+var Helloworld = React.createClass({
+    render: function() {
+        return (< h1 > hello pearyman < /h1>
+    )
+  }
+})
+
+ReactDOM.render(<Helloworld/ >, document.getElementById('example'));
