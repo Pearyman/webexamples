013c5069ca4dedda58ea37c0b1733fb4c18aec97
Pearyman <liyananhappy@sina.cn>
2016-08-24 17:30:17 +0800

    react onClick

--- js/helloworld.js
+++ js/helloworld.js
@@ -2,11 +2,31 @@
  * React commentBox test
  */
 
-var Helloworld = React.createClass({
-    render: function() {
-        return (< h1 > hello world < /h1>
+// var Helloworld = React.createClass({
+//     render: function() {
+//         return (< h1 > hello pearyman< /h1>
+//     )
+//   }
+// })
+//
+//
+//
+var ListSurvery=React.createClass({
+  render:function(){
+    var styles={
+      width: 100+'px',
+      height:100+'px',
+      backgroundColor:'green',
+      color: '#fff'
+    }
+    return (
+      <div style={styles} onClick={this.handleClick}>hahahaasdsdsd</div>
     )
-  }
-})
 
-ReactDOM.render(<Helloworld/ >, document.getElementById('example'));
+  },
+  handleClick:function(){
+    alert(66);
+  }
+});
+
+ReactDOM.render(<ListSurvery/>, document.getElementById('example'));
