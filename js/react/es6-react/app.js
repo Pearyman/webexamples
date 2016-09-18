a649ef4ccbb8ae704450bbd01df7cf0e6cba340a
Pearyman <liyananhappy@sina.cn>
2016-09-18 15:16:53 +0800

    delete some unuseful demo

--- js/react/es6-react/app.js
+++ js/react/es6-react/app.js
@@ -1,8 +1,25 @@
 import React from 'react';
 
 class App extends React.Component {
+    constructor(){
+      super();
+      this.state={
+        txt:'this is the state text',
+        cat:0
+      }
+    }
+
+    update(e){
+      this.setState({txt: e.target.value})
+    }
+
     render() {
-      return <h1> hello </h1>
+      return (
+        <div>
+          <input type="text"
+            onChange={this.update.bind(this)}/>
+          <h1> {this.state.txt} </h1>
+        </div>)
     }
 }
 
