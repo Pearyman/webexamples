45bef5b814b76165ee902cb4f558a89629f5deff
Pearyman <liyananhappy@sina.cn>
2016-09-27 18:07:08 +0800

    vue cdn test

--- js/vue/vue-test/src/components/Hello.vue
+++ js/vue/vue-test/src/components/Hello.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <ul>
+      <li v-for="todo in todos">{{todo.text}}</li>
+    </ul>
   </div>
 </template>
 
@@ -8,17 +10,16 @@
 export default {
   data () {
     return {
-      // note: changing this line won't causes changes
-      // with hot-reload because the reloaded component
-      // preserves its current state and we are modifying
-      // its initial state.
-      msg: 'Hello World!'
+      todos: [
+        { text: '123' },
+        { text: '123' },
+        { text: '123' }
+      ]
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1 {
   color: #42b983;
--- js/vue/vue-test/src/main.js
+++ js/vue/vue-test/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App'
-
+// import test from './test'
 /* eslint-disable no-new */
 new Vue({
   el: 'body',
-  components: { App }
+  components: {
+    App
+  }
 })
