9dc96da33515ba9aa656a298af6cbaa48e8639cf
Pearyman <liyananhappy@sina.cn>
2016-11-17 10:34:52 +0800

    node test

---
+++ js/node/node_test_01/app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var utility = require('utility');
+
+var app = express();
+
+app.get('/', function(req, res) {
+    var q = req.query.q;
+    var md5Value = utility.md5(q);
+    res.send(md5Value);
+});
+app.listen(3000, function(req, res) {
+    console.log('app is listening at port 3000');
+});
